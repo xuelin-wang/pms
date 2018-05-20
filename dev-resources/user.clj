e077dd5374778f1e933027c49d5d4f3422bb56f0
Xuelin Wang <xuwang@paloaltonetworks.com>
2018-05-20 13:22:57 -0700

    adapt from lacinio example but with integrate in stead of component

---
+++ dev-resources/user.clj
@@ -0,0 +1,106 @@
+(ns user
+  (:require
+    [pms.game.schema :as sc]
+    [pms.game.db :as db]
+    [com.walmartlabs.lacinia :as lacinia]
+    [com.walmartlabs.lacinia.pedestal :as lp]
+    [io.pedestal.http :as http]
+    [clojure.java.browse :refer [browse-url]]
+    [integrant.core :as ig]
+    [clojure.walk :as walk])
+  (:import (clojure.lang IPersistentMap)))
+
+;(def schema (sc/load-schema))
+
+(defn simplify
+  "Converts all ordered maps nested within the map into standard hash maps, and
+   sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
+  [m]
+  (walk/postwalk
+    (fn [node]
+      (cond
+        (instance? IPersistentMap node)
+        (into {} node)
+
+        (seq? node)
+        (vec node)
+
+        :else
+        node))
+    m))
+
+(comment
+  (defonce server nil)
+
+  (defn start-server
+    [_]
+    (let [server (-> schema
+                     (lp/service-map {:graphiql true})
+                     http/create-server
+                     http/start)]
+      ;  (browse-url "http://localhost:8888/")
+      server))
+
+  (defn stop-server
+    [server]
+    (http/stop server)
+    nil)
+
+  (defn start
+    []
+    (alter-var-root #'server start-server)
+    :started)
+
+  (defn stop
+    []
+    (alter-var-root #'server stop-server)
+    :stopped)
+  )
+
+(def sysconf
+  {
+   :server/jetty {:schema (ig/ref :game/cgg-schema)}
+   :game/cgg-schema {:db (ig/ref :game/db)}
+   :game/db {}
+   })
+
+(defmethod ig/init-key :game/db [_ _]
+  (db/new-db)
+  )
+
+(defmethod ig/halt-key! :game/db [_ _]
+  nil
+  )
+
+(defmethod ig/init-key :game/cgg-schema [_ {:keys [db]}]
+  (sc/load-schema db)
+  )
+
+(defmethod ig/halt-key! :game/cgg-schema [_ _]
+  nil
+  )
+
+(defmethod ig/init-key :server/jetty [_ {:keys [schema]}]
+  (let [server (-> schema
+                   (lp/service-map {:graphiql true})
+                   http/create-server
+                   http/start)]
+    ;  (browse-url "http://localhost:8888/")
+    server)
+  )
+
+(defmethod ig/halt-key! :server/jetty [_ server]
+  (http/stop server)
+  nil
+  )
+
+(def system (ig/init sysconf))
+
+(defn q
+  [query-string]
+  (let [schema (:game/cgg-schema system)]
+    (-> (lacinia/execute schema query-string nil nil)
+        simplify)
+    )
+  )
+
